42629d94a6a73b73b6086e0fd4fcb4bfce42eb0f
Douglas Correia <douglas.correia@iteris.com.br>
2020-01-03 17:09:22 -0300

    adaptando as cores dos botões

--- jobs/src/app/vagas/vagas-list.service.spec.ts
+++ jobs/src/app/vagas/vagas-list.service.spec.ts
@@ -1,12 +1,4 @@
 import { TestBed } from '@angular/core/testing';
 
-import { VagasListService } from './vagas-list.service';
 
-describe('VagasListService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: VagasListService = TestBed.get(VagasListService);
-    expect(service).toBeTruthy();
-  });
-});
