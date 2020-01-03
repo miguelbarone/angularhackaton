4ac593eb1da8ed320a36a330a162129c812ac6bb
Rafael Amorim <rafael.amorim@iteris.com.br>
2020-01-03 15:38:09 -0300

    filtrando vagas correto

--- jobs/src/app/vagas/vagas-list/vagas-list.component.ts
+++ jobs/src/app/vagas/vagas-list/vagas-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StoreService } from 'src/app/store/store.service';
 import { VagaService } from "../../Services/vaga.service";
 import { VagaModel } from 'src/app/store/vagas-model';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-vagas-list',
@@ -13,11 +14,25 @@ export class VagasListComponent implements OnInit {
   vagas: VagaModel[];
   vaga: VagaModel;
 
-  constructor(private store: StoreService, private vs: VagaService) { 
+  tipoContrato:string = "";
+  tecnologia:string = "";
+
+  constructor(private store: StoreService, private vs: VagaService, private ss: SearchService) { 
     this.vagas = store.vagas;
   }
 
   ngOnInit() {
+  }
+
+  filtrar(){
+
+    if(this.tipoContrato != "" && this.tecnologia != ""){
+      this.vagas = this.ss.filtrarPorContratoETecnologia(this.tecnologia, this.tipoContrato);
+      console.log(this.ss.filtrarPorContratoETecnologia(this.tecnologia, this.tipoContrato));
+    }else{
+      alert("porra nenhuma")
+    }
+  
   }
 
 }
--- jobs/src/app/vagas/vagas.module.ts
+++ jobs/src/app/vagas/vagas.module.ts
@@ -5,16 +5,15 @@ import { VagasListComponent } from './vagas-list/vagas-list.component';
 import { RouterModule } from '@angular/router';
 import { UtilModule } from '../util/util.module';
 import { VagaInfoComponent } from './vaga-info/vaga-info.component';
-
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [VagasListComponent, VagaInfoComponent,VagasCarrouselComponent],
   imports: [
     CommonModule,
     RouterModule,
-    UtilModule
+    UtilModule,
+    FormsModule
   ],
   exports: [VagasListComponent, VagaInfoComponent,VagasCarrouselComponent]
 })
