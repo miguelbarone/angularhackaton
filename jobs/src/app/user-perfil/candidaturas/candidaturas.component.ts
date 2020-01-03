9eec4cef81508b03aa714cc79ce55bd511c29e7d
luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-01-03 14:35:56 -0300

    Criado os componentes do perfil do usuario

---
+++ jobs/src/app/user-perfil/candidaturas/candidaturas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-candidaturas',
+  templateUrl: './candidaturas.component.html',
+  styleUrls: ['./candidaturas.component.css']
+})
+export class CandidaturasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- jobs/src/app/user-perfil/user-perfil.module.ts
+++ jobs/src/app/user-perfil/user-perfil.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserEditFormComponent } from './user-edit-form/user-edit-form.component';
+import { CandidaturasComponent } from './candidaturas/candidaturas.component';
+import { UserMenuComponent } from './user-menu/user-menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserEditFormComponent, CandidaturasComponent, UserMenuComponent],
   imports: [
     CommonModule
   ]
