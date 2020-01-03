7e017c5e4f39dc5f46c15bb9e5c5681c351e4e5b
Rafael Amorim <rafael.amorim@iteris.com.br>
2020-01-03 17:53:03 -0300

    finalizando tela de perfil

--- jobs/src/app/user-perfil/user-edit-form/user-edit-form.component.ts
+++ jobs/src/app/user-perfil/user-edit-form/user-edit-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { StoreService } from 'src/app/store/store.service';
+import { PfModel } from 'src/app/store/pf-model';
 
 @Component({
   selector: 'app-user-edit-form',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditFormComponent implements OnInit {
 
-  constructor() { }
+  formEdicao: FormGroup;
+
+  constructor(private store: StoreService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.formEdicao = this.fb.group({
+      nome: [this.store.usuario.nome, Validators.required],
+      email: [this.store.usuario.email, Validators.required],
+      senha: [this.store.usuario.senha, Validators.required],
+      foto: [this.store.usuario.foto, Validators.required],
+      sobre: [this.store.usuario.sobre, Validators.required]
+    })
+  }
+
+  atualizar(){
+    let atualizacao = new PfModel();
+    let id = this.store.usuario.id;
+    atualizacao.nome = this.formEdicao.get("nome").value;
+    atualizacao.email = this.formEdicao.get("email").value;
+    atualizacao.senha = this.formEdicao.get("senha").value;
+    atualizacao.foto = this.formEdicao.get("foto").value;
+    atualizacao.sobre = this.formEdicao.get("sobre").value;
+    atualizacao.candidaturas = this.store.usuario.candidaturas;
+
+    this.store.usuario = atualizacao;
   }
 
 }
