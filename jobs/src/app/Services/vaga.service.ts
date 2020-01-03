bb3c721bdd4e298778ebb2288007034654baef7a
luiz Faleiros <luiz.faleiros@iteris.com.br>
2020-01-03 10:57:16 -0300

    Arrumado o vagas carrousel nãp aparecer

--- jobs/src/app/Services/vaga.service.ts
+++ jobs/src/app/Services/vaga.service.ts
@@ -1,16 +1,19 @@
+import { StoreService } from './../store/store.service';
 import { Router } from '@angular/router';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { VagaModel } from '../store/vagas-model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class VagaService {
-  vagas: VagaModel[] = [];
+  vagas: VagaModel[];
+  constructor(private router: Router, private store: StoreService) {
+    this.vagas = this.store.vagas;
+  }
   vagaClicada: VagaModel;
-  constructor(private router: Router) { }
 
-  abrirVaga(vaga){
+  abrirVaga(vaga) {
     this.vagaClicada = vaga;
     this.router.navigate(['vaga-info']);
   }
